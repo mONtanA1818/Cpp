7084f9256614ec6810a42d88584707fcc5eeb13e
Haozheng Li <haozhengli893@gmail.com>
2024-04-26 16:48:02 +0800

    4.26

--- A04_class_object/B01_seal/B06.cpp
+++ A04_class_object/B01_seal/B06.cpp
@@ -12,13 +12,41 @@ public:
         m_W = w;
     }
 
-
-    int calculate_area(){
-        return 2*(m_H*m_L+m_L*m_W+m_W*m_H);
+    int calculate_area()
+    {
+        return 2 * (m_H * m_L + m_L * m_W + m_W * m_H);
     }
 
-    int calculate_Volumetric(){
-        return m_H*m_L*m_W;
+    int calculate_Volumetric()
+    {
+        return m_H * m_L * m_W;
+    }
+
+    int get_L()
+    {
+        return m_L;
+    }
+    int get_W()
+    {
+        return m_W;
+    }
+
+    int get_H()
+    {
+
+        return m_H;
+    }
+
+    bool isSame(Cube &c1)
+    {
+        if (m_H == c1.get_H() && m_L == c1.get_L() && m_W == c1.get_W())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
 private:
@@ -27,13 +55,53 @@ private:
     int m_H;
 };
 
+bool isSame(Cube &c1, Cube &c2)
+{
+    if (c2.get_H() == c1.get_H() && c2.get_L() == c1.get_L() && c2.get_W() == c1.get_W())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
-
-int main(){
+int main()
+{
     Cube c1;
-    c1.setCube(1,2,3);
-    cout<<"c1的表面积为"<<c1.calculate_area()<<endl;
-    cout<<"c1的体积为"<<c1.calculate_Volumetric()<<endl;
+    c1.setCube(1, 2, 3);
+    Cube c2;
+    c2.setCube(1,3,3);
+    cout << "c1的表面积为" << c1.calculate_area() << endl;
+    cout << "c1的体积为" << c1.calculate_Volumetric() << endl;
 
+    cout<<"这是全局函数"<<endl;
+    // 全局函数判断这两个正方体是否相等
+    bool ret = isSame(c1,c2);
+    if (ret)
+    {
+        cout<<"这两个正方体是相等的"<<endl;
+    }else
+    {
+        cout<<"这两个正方体不是相等的"<<endl;
+
+    }
+
+    cout<<"============================="<<endl;
+    // 使用局部函数判断两个正方体是否相等
+
+    
+    cout<<"这是局部函数"<<endl;
+    bool ret2 = c1.isSame(c2);
+    if (ret2)
+    {
+        cout<<"这两个正方体是相等的"<<endl;
+    }else
+    {
+        cout<<"这两个正方体不是相等的"<<endl;
+
+    }
+    cout<<"+==========================="<<endl;
     return 0;
 }
